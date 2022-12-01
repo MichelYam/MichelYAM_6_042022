31025e5717244f7b38fd8a00f790fc5994b4f832
MichelYAM <michel.yam92@gmail.com>
2022-12-02 00:32:23 +0100

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,138 @@
+// const path = require('path');
+// const ESLintPlugin = require('eslint-webpack-plugin');
+// const HtmlWebPackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// // const glob = require('glob');
+
+// // const dev = process.env.NODE_ENV === 'dev';
+
+// const config = {
+//   // entry: path.resolve(__dirname, './src'),
+//   entry: [
+//     './src/pages/index.js',
+//     './src/pages/photographer.js',
+//     './src/factories/lightBox.js',
+//     './src/factories/media.js',
+//     './src/factories/photographer.js',
+//     './src/utils/contactForm.js',
+//     './src/api/api.js',
+//   ],
+//   output: {
+//     path: path.resolve(__dirname, './dist'),
+//     filename: 'bundle.js',
+//   },
+//   devServer: {
+//     static: path.resolve(__dirname, './dist'),
+//     open: true,
+//     port: 4000,
+//   },
+//   // devtool: dev ? 'cheap-module-eval-source-map' : 'source-map',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: ['babel-loader'],
+//       },
+//       {
+//         test: /\.(scss|css)$/,
+//         exclude: /node_modules/,
+//         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+//       },
+//       {
+//         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//         type: 'asset/resource',
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new ESLintPlugin(),
+//     new CleanWebpackPlugin(),
+//     new HtmlWebPackPlugin({
+//       filename: 'index.html',
+//       template: 'public/index.html',
+//       chunks: ['main'],
+//     }),
+//     new HtmlWebPackPlugin({
+//       filename: 'photographer.html',
+//       template: 'public/photographer.html',
+//       chunks: ['exampleEntry'],
+//     }),
+//     new MiniCssExtractPlugin(),
+//   ],
+
+//   // mode: 'development',
+// };
+
+// module.exports = config;
 const path = require('path');
-const ESLintPlugin = require('eslint-webpack-plugin');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const glob = require('glob');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-// const dev = process.env.NODE_ENV === 'dev';
-
-const config = {
-  // entry: path.resolve(__dirname, './src'),
-  entry: [
-    './src/pages/index.js',
-    './src/pages/photographer.js',
-    './src/factories/lightBox.js',
-    './src/factories/media.js',
-    './src/factories/photographer.js',
-    './src/utils/contactForm.js',
-    './src/api/api.js',
-  ],
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, 'src/pages/index.js'),
+    photographer: path.resolve(__dirname, 'src/pages/photographer.js'),
+  },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
   },
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-    open: true,
-    port: 4000,
-  },
-  // devtool: dev ? 'cheap-module-eval-source-map' : 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
-        test: /\.(scss|css)$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
-    new ESLintPlugin(),
     new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
+    new ESLintPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './public/assets',
+          to: 'assets',
+        },
+        {
+          from: './public/css',
+          to: 'css',
+        },
+        {
+          from: './data/photographers.json',
+          to: 'data',
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'public/index.html',
-      chunks: ['main'],
+      template: path.resolve(__dirname, 'public/index.html'),
+      chunks: ['index'],
+      scriptLoading: 'module',
     }),
-    new HtmlWebPackPlugin({
+    new HtmlWebpackPlugin({
       filename: 'photographer.html',
-      template: 'public/photographer.html',
-      chunks: ['exampleEntry'],
+      template: path.resolve(__dirname, 'public/photographer.html'),
+      chunks: ['photographer'],
+      scriptLoading: 'module',
     }),
-    new MiniCssExtractPlugin(),
   ],
-
-  // mode: 'development',
+  devtool: 'source-map',
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    port: 4000,
+  },
 };
-
-// if (!dev) {
-//   config.plugins.push(new UglifyJSPlugin({
-//     sourceMap: true,
-//   }));
-// }
-// const files = glob.sync(`${process.cwd()}/public/*.html`);
-
-// files.forEach((element) => {
-//   config.plugins.push(new HtmlWebPackPlugin({
-//     template: element,
-//   }));
-// });
-module.exports = config;
